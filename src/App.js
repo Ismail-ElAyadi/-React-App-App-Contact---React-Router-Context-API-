21e7de7a070d4d841c8ab61b015e82b66481282e
ismail <ismail.elydi@gmail.com>
2020-01-24 10:20:36 +0100

    Ajout du Provider dans AppJs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './composants/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./composants/Header";
 import Liste from "./composants/Liste";
+import { Provider } from "./context";
 
 class App extends Component {
-
-
   render() {
-
     return (
-      <div className="App">
-        <Header/>
-       <div className="container">
-       <Liste/>
-       </div> 
-      </div>
+      <Provider>
+        <div className="App">
+          <Header />
+
+          <div className="container">
+            <Liste />
+          </div>
+        </div>
+      </Provider>
     );
   }
 }
--- src/context.js
+++ src/context.js
@@ -1,39 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-const Context = React.createContext
+// Pour créer le context
+const Context = React.createContext();
 
-//
+//Provider = celui qui donne en Anglais
 export class Provider extends Component {
-
-
-    state = {
-        infoContacts: [
-          {
-            id: 1,
-            nom: "René Doe",
-            email: "jhi@gmail.com",
-            tel: "655-555-555"
-          },
-          {
-            id: 2,
-            nom: "Legende Doe",
-            email: "jhi@gmail.com",
-            tel: "655-555-555"
-          },
-          {
-            id: 3,
-            nom: "Isma",
-            email: "jhi@gmail.com",
-            tel: "655-555-555"
-          }
-        ]
+  // on Recupere le state du component Liste
+  state = {
+    infoContacts: [
+      {
+        id: 1,
+        nom: "René Doe",
+        email: "jhi@gmail.com",
+        tel: "655-555-555"
+      },
+      {
+        id: 2,
+        nom: "Legende Doe",
+        email: "jhi@gmail.com",
+        tel: "655-555-555"
+      },
+      {
+        id: 3,
+        nom: "Isma",
+        email: "jhi@gmail.com",
+        tel: "655-555-555"
       }
-
-      render(){
-return (<Context.Provider value ={this.state}>
-{this.props.childred}
-</Context.Provider>)
-      }
+    ]
+  };
+  render() {
+    return (
+      //On Prend tout le state
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
 }
-
-export const Consumer=Context.Consumer
+//Consumer = Utilisateur
+export const Consumer = Context.Consumer;
